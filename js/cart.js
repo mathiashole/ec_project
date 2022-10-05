d8470ca797bc7bf363fd0de5a2380ec9cde85bae
mathiashole <joacomangino@gmail.com>
2022-10-05 16:01:06 -0300

    We finished fixing cart

--- js/cart.js
+++ js/cart.js
@@ -35,21 +35,23 @@ const setCart = () => {
 
         cart[product.id] = {...product}
 
-        console.log(product)
+        console.log(product.unitCost)
+        return(product)
 
 }
 
-const showCart = () => {
 
-    cat();   
+
+const showCart = () => {
+   
     itemsCart.innerHTML = ''
     presentCartArray.articles.forEach(product => {
         templateCart.querySelector('img').setAttribute('src', product.image)
         templateCart.querySelectorAll('td')[0].textContent =product.name
         templateCart.querySelectorAll('td')[1].textContent = product.currency + " " + product.unitCost
-        // templateCart.querySelector('input').setAttribute('value', product.count) 
-        templateCart.querySelector('.btn-success').dataset.id = product.id
-        templateCart.querySelector('.btn-danger').dataset.id = product.id
+        templateCart.querySelector('input').setAttribute('value', product.count) 
+        // templateCart.querySelector('.btn-success').dataset.id = product.id
+        // templateCart.querySelector('.btn-danger').dataset.id = product.id
         templateCart.querySelectorAll('b')[0].textContent =  product.currency
         templateCart.querySelectorAll('b')[1].textContent =  product.count * product.unitCost
 
@@ -72,67 +74,15 @@ const showFooter = () => {
 
 }
 
-// function showCartList() {
-    
-//     let cartToAppend = "";
-//     let itemCart = presentCartArray;
+function cat(){
+    window.addEventListener("input", function(event) {
 
-//     cartToAppend += `
-//     <div class="col mb-4">
-//         <div class="d-flex justify-content-center mx-5 mt-4">
-//             <h1 class="mb-1">Carrito de compras</h1>
-//         </div>
-//         <div class="col mt-4">
-//             <div class="col">
-//                 <h4 class="d-flex w-100 justify-content-between mb-4">Articulos a comprar</h4>
-//             </div>
-//         </div>
-//         <table class="table">
-//           <thead class="table-dark text-center">
-//             <tr>
-//               <th scope="col"></th>
-//               <th scope="col">Nombre</th>
-//               <th scope="col">Costo</th>
-//               <th scope="col">Cantidad</th>
-//               <th scope="col">Subtotal</th>
-//             </tr>
-//           </thead>
-//           <tbody id="contenedor">
-//         `
-//         for(let i = 0; i < presentCartArray.articles.length; i++){
-//             let infoCart = presentCartArray.articles[i];
-            
-//             cartToAppend +=`
-//             <tr class="text-center">
-//                 <th>
-//                     <button type="button" class="btn"><img src="${infoCart.image}" class="rounded-3" width="90" height="60"></button>          
-//                 </th>
-//                 <th>
-//                     <p>${infoCart.name}</p>          
-//                 </th>
-//                 <th>              
-//                     <p>${infoCart.currency + " " + infoCart.unitCost}</p>          
-//                 </th>
-//                 <th class="col-1">
-//                 <input type="number" name="cartCostInput" class="form-control" id="cartCount" value=${infoCart.count} min="1">          
-//                 </th>
-//                 <th>
-//                     <b id="kek">${infoCart.currency + " " + infoCart.unitCost}</b>          
-//                 </th>
-//             </tr>    
-//             `
-
-//         }
-
-//         cartToAppend +=`
-//         </tbody>
-//         </table>  
-//     </div>
-//     `
-
-//     document.getElementById("cart-container").innerHTML = cartToAppend;
-
-// }
+    const init = presentCartArray.articles[0]
+    const numberObject = document.getElementById("cartCount").value;
+    console.log(init.unitCost*numberObject)
+    document.getElementById('totalCost') .innerHTML = init.unitCost * numberObject 
+        
+});}
 
 
 
@@ -142,7 +92,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(CART_USER).then(function (resultObj) {
         if (resultObj.status === "ok") {
             presentCartArray = resultObj.data
-            setCart();
+            cat();
             showCart();
         }
     });
@@ -153,15 +103,6 @@ document.addEventListener("DOMContentLoaded", function (e) {
 
 });
 
-
-function cat(){
-    window.addEventListener("input", function(event) {
-   
-    const numberObject = document.getElementById("cartCount");
-    console.log(numberObject.value)
-    return(parseInt(numberObject.value))    
-        
-});}
 
 // function showNumber(){
 //     document.getElementById("cartCount").value;
